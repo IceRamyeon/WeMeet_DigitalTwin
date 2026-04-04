5aad3c2283a99ac394037c91c5873d25ea0a6cd5
IceRamyeon <bladenova01@gmail.com>
2026-04-04 23:35:35 +0900

    coding NN

--- AI_NN_examples/sketch1.py
+++ AI_NN_examples/sketch1.py
@@ -1,1 +1,23 @@
+# %% 1. importing necessary libraries
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# %% 2. loading the MNIST dataset
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+trainf_features, train_labels = next(iter(trainloader))
+print("The shape of the training features is:", trainf_features.shape)
+print("The shape of the training labels is:", train_labels.shape)
+
